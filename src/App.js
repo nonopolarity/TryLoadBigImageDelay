3f52d70418229812b3d636a25b01c79b76a3a939
Kin <winterheat@gmail.com>
2023-03-09 04:32:30 -0800

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [toggle, setToggle] = useState(true);
+
+  const imageFilename = `${toggle ? "pic1.jpg" : "pic2.jpg"}?${Date.now()}`;
+
   return (
     <div className="App">
-      hi there
+      <div id="msg">
+        imageFilename is {imageFilename}
+        <button onClick={() => setToggle((f) => !f)}>Toggle</button>
+      </div>
+      <img src={imageFilename} />
     </div>
   );
 }
